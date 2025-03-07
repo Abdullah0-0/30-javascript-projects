4494cbece6bebf891645a9468465f26176a35d90
Abdullah0-0 <Abdullahjan161206@gmail.com>
2025-03-07 23:43:48 +0500

    2 done

---
+++ 2/script.js
@@ -0,0 +1,17 @@
+
+// // Static Numbers 
+
+// let a = 4;
+// let b = 6;
+// let c = a + b;
+// // console.log("The sum of "+  a  +" + " +  b  + " = " + c);
+// console.log(`The sum of ${a} + ${b} =  ${c}`);
+
+// Daynamic Numbers 
+
+// let a = Number(prompt("Enter a number"))
+// let b = Number(prompt("Enter another number"))
+
+// const c = a + b
+// console.log(c);
+ 
